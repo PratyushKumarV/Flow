cd9aadbd8eeb0114e88552ff8689c307c7143ce2
Pratyush <pratyushkumarv@gmail.com>
2025-04-12 12:59:47 +0530

    basic landing page

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,13 @@
+function Title(){
+    return (
+        <>
+            <div className="title-wrapper">
+                <div className="title">
+                    FLOW
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Title
