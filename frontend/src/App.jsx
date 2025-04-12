93ee3f3def6eba8569165501a6cc501cc3c23d16
Pratyush <pratyushkumarv@gmail.com>
2025-04-12 21:34:59 +0530

    added a basic dashboard structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Title from './components/Title'
+import Dashboard from './components/Dashboard'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Title />
+      <Dashboard />
     </>
   )
 }
---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,30 @@
+function Dashboard(){
+    return (
+        <>
+            <div className="dashboard-wrapper">
+                <div className="dashboard">
+                    <div className="dashboard-main">
+                        <div className="dashboard-main-today-wrapper">
+                            <div className="dashboard-main-today-container">
+                                <div className="dashboard-main-day">
+                                    Sunday
+                                </div>
+                                <div className="dashboard-main-time">
+                                    12:30 pm
+                                </div>
+                            </div>
+                            <div className="dashboard-main-day-icon">
+                                ☀️
+                            </div>
+                        </div>
+                        <div className="dashboard-main-options">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
