e349cbb3fb401c57ab4e2aa3cbeda035f90c437d
Pratyush <pratyushkumarv@gmail.com>
2025-04-18 00:09:46 +0530

    Implemented date functionality and improved frontend design and fonts

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,4 +1,9 @@
 function Dashboard(){
+
+    const date=new Date()
+    const emojiList=['🌄', '🌅', '🌤️', '⛅', '🌞', '🌟', '☀️']
+    const emoji=emojiList[date.getDay()]
+
     return (
         <>
             <div className="dashboard-wrapper">
@@ -7,22 +12,41 @@ function Dashboard(){
                         <div className="dashboard-main-today-wrapper">
                             <div className="dashboard-main-today-container">
                                 <div className="dashboard-main-day">
-                                    Sunday
+                                    {date.toLocaleString('en-us', {weekday:'long'})}
                                 </div>
-                                <div className="dashboard-main-time">
-                                    12:30 pm
+                                <div className="dashboard-main-date">
+                                    {date.toLocaleString('en-IN', {month:'long'})} {""}
+                                    {date.toLocaleString('en-IN', {day:'2-digit'})} {", "}
+                                    {date.toLocaleString('en-IN', {year:'numeric'})}
                                 </div>
                             </div>
                             <div className="dashboard-main-day-icon">
-                                ☀️
+                                {emoji}
                             </div>
                         </div>
                         <div className="dashboard-main-options">
-
+                            <div className="dashboard-main-options-due">
+                                #4 Tasks due today
+                            </div>
+                            <div className="dashboard-main-options-new">
+                                <button>
+                                    New +
+                                </button>
+                            </div>
+                            <div className="dashboard-main-options-filter">
+                                <select>
+                                    <option>Filter</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div className="tasks-wrapper">
+
+            </div>
+
         </>
     )
 }
