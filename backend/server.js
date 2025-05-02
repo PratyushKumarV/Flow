8f35a0a2837a48dfd90150a452bbf97f53e65ad8
Pratyush <pratyushkumarv@gmail.com>
2025-05-02 23:57:18 +0530

    changed cors allowed origins again

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT=process.env.PORT
 const app=express()
 
 app.use(cors({
-    origin: ["http://localhost:5173","https://flow-three-black.vercel.app"],
+    origin: ["http://localhost:5173","https://flow-three-black.vercel.app", "https://flow-pratyushkumarvs-projects.vercel.app/"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"]
 }))
